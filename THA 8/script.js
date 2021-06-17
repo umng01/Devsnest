44c7954a2599712feb98b8df6deb9fbd5d1904df
Umang Sharma <umng01@gmail.com>
2021-06-17 12:44:24 +0530

    Blocks

---
+++ THA 8/script.js
@@ -0,0 +1,23 @@
+var Grid = document.querySelector('.grid');
+window.onload = () => {
+    for (let i=0;i<16;i++){
+        var row = document.createElement('div');
+        row.className = 'row';
+        Grid.appendChild(row);
+
+        for (let j=0;j<16;j++){
+            var col = document.createElement('div');
+            col.className = 'col';
+            row.appendChild(col);
+        }
+    }
+}
+
+setTimeout(() => {
+    var cols = document.querySelectorAll('.col');
+    cols.forEach(col => {
+        col.addEventListener('click', () => {
+            col.classList.toggle('white');
+        });
+    });
+}, 100)

1d555f84f5ed77b69eaf650421c8b50043e10cf0
Umang Sharma <umng01@gmail.com>
2021-07-12 15:45:04 +0530

    levelup

---
+++ THA19/counting/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Clicking from './buttons';
+export default function App() {
+  return (
+    <div className="App">
+      <Clicking/>
+      <Clicking/>
+      <Clicking/>
+      <Clicking/>
+    </div>
+  );
+}

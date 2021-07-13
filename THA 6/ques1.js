c5623ac73da41dcfa69e57578bb44e92a2cc30c0
Umang Sharma <umng01@gmail.com>
2021-07-13 20:43:51 +0530

    React

--- THA 6/ques1.js
+++
@@ -1,5 +0,0 @@
-// Write a JavaScript program to list the properties of a JavaScript object. 
-//Sample object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
-//Sample Output: name,sclass,rollno
-var student = {name : "David Rayy", sclass : "VI", rollno : 12 };
-console.log(student);

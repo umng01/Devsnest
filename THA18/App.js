26caee022e3e13df1011ea5df19ca04e62424c2e
Umang Sharma <umng01@gmail.com>
2021-07-13 21:42:26 +0530

    Devsnest Frontend

---
+++ THA18/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+function wallpper() {
+  return (
+    <div>
+      <img src="https://wallpaperaccess.com/full/1630412.jpg" alt="" />
+    </div>
+  );
+}
+
+function Box(){
+    return <div className="box"></div>
+  };
+  function Row(){
+    return (
+      <div className="row">
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box> 
+        <Box></Box>
+        
+       </div> 
+    );
+  };
+  
+  function blacknwhite(){
+    return (
+      <div>
+        <div className="block">
+          <Row></Row>
+          <Row></Row>
+          <Row></Row>
+          <Row></Row>
+          <Row></Row>
+          <Row></Row>
+          <Row></Row>
+          <Row></Row> 
+           
+        </div>
+       </div> 
+    );
+  };

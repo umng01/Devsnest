26a9526d8a707987f88477b2209abb16de4290d3
Umang Sharma <umng01@gmail.com>
2021-07-20 03:56:06 +0530

    Calorie card with more functionalities

---
+++ THA21/AddItem.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import './App.css';
+
+export default function AddItemForm({ addItem }) {
+	const [title, setTitle] = useState('');
+	const [calorie, setCalorie] = useState('');
+
+	function Submitting(e){
+		e.preventDefault();
+		addItem({ title, calorie });
+		setTitle('');
+		setCalorie('');
+	};
+
+	return (
+		<form onSubmit={Submitting}>
+			<label className='title'>Title:</label>
+			<input
+				type='text'
+				name='title'
+				id='title'
+				required
+				value={title}
+				onChange={e => setTitle(e.target.value)}
+				placeholder='Item name'
+			/>
+			<label className='title'>Calories:</label>
+			<input
+				type='number'
+				name='number'
+				id='number'
+				required
+				value={calorie}
+				onChange={e => setCalorie(e.target.value)}
+				placeholder='Calorie amount'
+			/>
+			<button type='submit' className='submit'>Add to list</button>
+		</form>
+	);
+}
